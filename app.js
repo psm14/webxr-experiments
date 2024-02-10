8e76c346fc09f7406e18cfe54224e9fe9e76072f
Patrick McLaughlin <me@patmclaughl.in>
2024-02-10 17:37:13 -0500

    add webxr boilerplate

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // Import the necessary libraries
-import * as THREE from "https://unpkg.com/three@0.125.2/build/three.module.js";
+import * as THREE from "https://unpkg.com/three@0.161.0/build/three.module.js";
+
+const vrButton = document.getElementById("enter-vr");
 
 // Create a scene
 const scene = new THREE.Scene();
@@ -16,6 +18,7 @@ camera.position.z = 5;
 // Create a renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.xr.enabled = true;
 document.body.appendChild(renderer.domElement);
 
 // Create a cube
@@ -24,17 +27,46 @@ const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
+cube.position.set(0, 3, -5);
+
+async function initXR() {
+  // WebXR
+  if ("xr" in navigator) {
+    if (await navigator.xr.isSessionSupported("immersive-vr")) {
+      vrButton.addEventListener("click", async () => {
+        const session = await navigator.xr.requestSession("immersive-vr", {
+          requiredFeatures: ["local-floor"],
+          optionalFeatures: ["bounded-floor"],
+        });
+        console.log(session);
+        renderer.xr.setSession(session);
+      });
+    } else {
+      vrButton.disabled = true;
+      vrButton.innerText = "VR not supported";
+    }
+  } else {
+    vrButton.disabled = true;
+    vrButton.innerText = "VR not supported";
+  }
+}
+
+console.log("hi");
+
 // Animation loop
 function animate() {
-  requestAnimationFrame(animate);
+  renderer.setAnimationLoop(function () {
+    // Rotate the cube
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
 
-  // Rotate the cube
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-
-  // Render the scene
-  renderer.render(scene, camera);
+    // Render the scene
+    renderer.render(scene, camera);
+  });
 }
 
 // Start the animation loop
 animate();
+initXR().catch(function (err) {
+  console.error(err);
+});
